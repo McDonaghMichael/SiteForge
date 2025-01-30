b1fcdd4a166c1a7d4d591fa3797f37895af148cc
McDonaghMichael <G00421944@atu.ie>
2025-01-30 17:14:40 +0000

    a

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
 func main() {
 
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleHome)
+	http.ListenAndServe(":8080", r)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
