2d8fcfea92804dea4a2657538fb76300d77d95b1
McDonaghMichael <G00421944@atu.ie>
2025-02-05 16:51:09 +0000

    Implemented method to fetch the html from mongodb based on the id

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"backend/methods"
 	"backend/routes"
+	"fmt"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -24,13 +26,20 @@ func main() {
 	routes.CreatePage(client, "Title", "<h1>r</h1>", "test-page", 0, ".png", "title-meta", "desc-meta", "cool blog, test")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleHome)
+	r.HandleFunc("/", handleHome(client))
 	r.HandleFunc("/page/slug/{slug}", routes.FindPageBySlug(client))
 	r.HandleFunc("/page/id/{id}", routes.FindPageById(client))
 	r.HandleFunc("/user/id/{id}", routes.FetchUserByID(client))
 	http.ListenAndServe(":8080", r)
 }
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+func handleHome(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		html, _ := methods.FindPageById(client, "67a3786977c8d8dad1fa1b38")
+		fmt.Fprint(w, html)
+	}
+
 }
