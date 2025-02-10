4db31aa25d8c9e68b067d431fd9cfc334385e908
McDonaghMichael <G00421944@atu.ie>
2025-02-10 21:57:50 +0000

    Created a BasePage.js which acts as a template for dynamic routes

--- app/src/pages/admin/pages/edit/EditPage.js
+++ app/src/pages/admin/pages/edit/EditPage.js
@@ -1,7 +1,35 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export default function EditPage () {
+
+    const [page, setPage] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/page/id/" + id).then(res => {
+            setPage(res.data);
+            console.log(page);
+        }).catch(error => {
+            console.error("Error fetching page:", error);
+        });
+    }, []);
+
     return (
         <>
-            <h1>Example</h1>
+            <form>
+                <label htmlFor="title">Page Title</label><br/>
+                <input type="text" id="title" name="title" value={page.title}/><br/>
+                <label htmlFor="slug">Slug</label><br/>
+                <input type="text" id="slug" name="slug" value={page.slug}/><br/>
+                <label htmlFor="html">HTML</label><br/>
+                <textarea type="text" id="html" name="html" value={page.html}/><br/>
+                <label htmlFor="css">Custom CSS</label><br/>
+                <textarea type="text" id="css" name="css" value={page.css}/><br/>
+                <label htmlFor="css">Featured Image</label><br/>
+
+            </form>
         </>
     )
 }
--- app/src/pages/global/base/BasePage.js
+++ app/src/pages/global/base/BasePage.js
@@ -1,6 +1,22 @@
+import DocumentMeta, {render} from 'react-document-meta';
+
 export default function BasePage ({theme, page}) {
+
+        const meta = {
+            title: page.meta_title,
+            description: page.meta_description,
+            canonical: 'http://example.com/path/to/page',
+            meta: {
+                charset: 'utf-8',
+                name: {
+                    keywords: page.meta_keywords
+                }
+            }
+        };
+
     return (
         <>
+            <DocumentMeta {...meta}>
             <style dangerouslySetInnerHTML={{ __html: theme.css }}>
             </style>
             <div dangerouslySetInnerHTML={{ __html: theme.navbar }}>
@@ -9,6 +25,7 @@ export default function BasePage ({theme, page}) {
             </div>
             <div dangerouslySetInnerHTML={{ __html: theme.footer }}>
             </div>
+            </DocumentMeta>
         </>
     )
 }
