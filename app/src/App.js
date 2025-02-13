f8936d15e1e58214eb44f2791766f669eb40a827
McDonaghMichael <G00421944@atu.ie>
2025-02-13 17:16:00 +0000

    Implemented Tailwindcss

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import './App.css';
+import './tailwind.css';
 import {Route, Routes} from "react-router-dom";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
 import ViewPages from "./pages/admin/pages/ViewPages";
--- app/src/pages/admin/dashboard/Dashboard.js
+++ app/src/pages/admin/dashboard/Dashboard.js
@@ -1,11 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Button from 'react-bootstrap/Button';
 
 export default function Dashboard (){
     return (
         <>
-            <h1>Dashboard</h1>
-            <Button/>
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+                <p className="text-gray-700">Your Tailwind CSS setup is successful!</p>
+            </div>
         </>
     )
 }
