90063b1780b906d69711e7b74d9901fa3b4b71db
McDonaghMichael <G00421944@atu.ie>
2025-02-13 17:22:22 +0000

    d

--- app/src/pages/admin/dashboard/Dashboard.js
+++ app/src/pages/admin/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Sidebar from "../components/sidebar/Sidebar";
 
 export default function Dashboard (){
     return (
         <>
+            <Sidebar/>
             <h1>Dashboard</h1>
             <Button/>
         </>
