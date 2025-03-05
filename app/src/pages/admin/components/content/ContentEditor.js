43739ba1f8c6cc14a6da6602a92a178fff00bed6
McDonaghMichael <G00421944@atu.ie>
2025-03-05 12:44:02 +0000

    Added H1,H2,H3,H4,H5, unordered and ordered list to the ContentEditor.js

--- app/src/pages/admin/components/content/ContentEditor.js
+++ app/src/pages/admin/components/content/ContentEditor.js
@@ -41,6 +41,41 @@ export default function ContentEditor({form, onChange, html}) {
         setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
     };
 
+    const onH1Click = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "header-one"));
+    };
+
+    const onH2Click = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "header-two"));
+    };
+
+    const onH3Click = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "header-three"));
+    };
+
+    const onH4Click = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "header-four"));
+    };
+
+    const onH5Click = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "header-five"));
+    };
+
+    const onUnorderedListClick = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "unordered-list-item"));
+    };
+
+    const onOrderedListClick = (e) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, "ordered-list-item"));
+    };
+
     useEffect(() => {
         const contentHtml = stateToHTML(editorState.getCurrentContent());
 
@@ -62,8 +97,15 @@ export default function ContentEditor({form, onChange, html}) {
             <button onClick={onBoldClick} className="btn btn-primary me-3 mb-1">Bold</button>
             <button onClick={onUnderlineClick} className="btn btn-primary me-3 mb-1">Underline</button>
             <button onClick={onItalicClick} className="btn btn-primary me-3 mb-1">Italic</button>
+            <button onClick={onOrderedListClick} className="btn btn-primary me-3 mb-1">Ordered List</button>
+            <button onClick={onUnorderedListClick} className="btn btn-primary me-3 mb-1">Unordered List</button>
+            <button onClick={onH1Click} className="btn btn-primary me-3 mb-1">H1</button>
+            <button onClick={onH2Click} className="btn btn-primary me-3 mb-1">H2</button>
+            <button onClick={onH3Click} className="btn btn-primary me-3 mb-1">H3</button>
+            <button onClick={onH4Click} className="btn btn-primary me-3 mb-1">H4</button>
+            <button onClick={onH5Click} className="btn btn-primary me-3 mb-1">H5</button>
             <div style={{border: "1px solid #ccc", padding: "10px", minHeight: "150px"}}>
-                <Editor editorState={editorState} onChange={setEditorState} />
+                <Editor editorState={editorState} onChange={setEditorState}/>
             </div>
         </div>
     );
