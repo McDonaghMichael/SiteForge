8ab9b04921cf3bd3c3397b41dc9756ca2c37af54
McDonaghMichael <G00421944@atu.ie>
2025-03-18 20:58:23 +0000

    Fixed theme view not replacing [ITEMS]

--- app/src/pages/admin/components/themes/ThemePreview.js
+++ app/src/pages/admin/components/themes/ThemePreview.js
@@ -2,7 +2,12 @@ export default function ThemePreview({ css, html }) {
     return (
         <div className="theme-container">
             <style dangerouslySetInnerHTML={{ __html: `.theme-container { ${css} }` }} />
-            <div dangerouslySetInnerHTML={{ __html: html}} />
+
+            {html && (
+                <div dangerouslySetInnerHTML={{__html: html.replace("[ITEMS]", "Homepage")}}/>
+            )}
+
+
         </div>
     );
 }
