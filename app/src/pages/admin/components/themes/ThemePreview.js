9170f1388623e4372e33c9db5c754728b02d68f4
McDonaghMichael <G00421944@atu.ie>
2025-04-18 11:08:12 +0100

    Fixed CSS Error

--- app/src/pages/admin/components/themes/ThemePreview.js
+++ app/src/pages/admin/components/themes/ThemePreview.js
@@ -1,20 +1,23 @@
 export default function ThemePreview({ css, html }) {
     return (
-        <div className="theme-container">
-            <style dangerouslySetInnerHTML={{ __html: `.theme-container { ${css.replace("fixed", "static")} }` }} />
+      <div className="theme-container">
+        {css && (
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `.theme-container { ${css.replace("fixed", "static")} }`,
+            }}
+          />
+        )}
 
-            {html && (
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: html
-                            .replace(
-                                "[ITEMS]", "Homepage")
-                            .replace("[SITE_TITLE]", "SiteForge"),
-                    }}
-                ></div>
-            )}
-
-
-        </div>
+        {html && (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: html
+                .replace("[ITEMS]", "Homepage")
+                .replace("[SITE_TITLE]", "SiteForge"),
+            }}
+          ></div>
+        )}
+      </div>
     );
 }
